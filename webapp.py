c50df5aae27eba86577706b4e1c07894db50f711
Jessica <jessica.lee124@gmail.com>
2018-07-14 12:29:46 -0400

    Flask

--- webapp.py
+++ webapp.py
@@ -1,6 +1,18 @@
-from flask import Flask
+# import the Flask class from the flask module
+from flask import Flask, render_template
+
+# create the application object
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return "Hello, World!"  # return a string
+
+@app.route('/welcome')
+def welcome():
+    return render_template('index.html')  # render a template
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
